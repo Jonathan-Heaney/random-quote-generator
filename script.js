d9cc399b529d9a387a07bb506f0fe56fd0dda286
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-11-04 11:53:05 -0500

    Add comments

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const speakerEl = document.getElementById('speaker');
 const quoteBtn = document.getElementById('quoteBtn');
 const dropdown = document.getElementById('tags');
 
+// Generate the quote upon loading the page
 generateQuote();
 
+// Generate a new quote every time you click the 'New Quote' button
 quoteBtn.addEventListener('click', generateQuote);
 
+// Generate a new quote- if no tags are selected (default), then a random quote of any type will be shown. If a tag is selected, only quotes of that tag will be shown
 async function generateQuote() {
   const value = dropdown.options[dropdown.selectedIndex].value;
   if (value === 'none') {
@@ -15,16 +18,17 @@ async function generateQuote() {
     quoteEl.textContent = data.content;
     speakerEl.textContent = `- ${data.author}`;
   } else {
+    // You can use template literals with URLs
     const res = await fetch(`https://api.quotable.io/random?tags=${value}`);
     const data = await res.json();
     quoteEl.textContent = data.content;
     speakerEl.textContent = `- ${data.author}`;
   }
-  // Don't forget the () for the .json()- it's a method
 }
 
 const tags = [];
 
+// Function to get a full list of tags from the API and display them
 async function listTags() {
   const res = await fetch('https://api.quotable.io/tags');
   const data = await res.json();
